f483c2df0b7f494baff874f937379ed67c7c8977
Lou Lee <Lou7532@hotmail.com>
2017-03-27 18:33:06 -0400

    towns

---
+++ town/TownsBuilding.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace town
+{
+    class TownsBuilding
+    {
+        public string Name { get; set; }
+        
+        public string Displayname()
+        {
+            return Name;
+        }
+        public override string ToString()
+        {
+            return $"house; {this.Name}";
+        }
+    }
+}
